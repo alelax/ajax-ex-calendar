5e9e1e2076207524621e1fbd361f4d87f009603b
alelax <alessandro.lausdei@gmail.com>
2018-04-23 17:04:25 +0200

    Ex Calendar - new version

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 
    var country = "IT";
    var startDay = "01";
-   var startMonth = moment().month(0).format("MM");
+   var month = moment().month(0);
+   var startMonth = month.format("MM");
    var startYear = 2017;
 
    var format = startYear+"-"+startMonth+"-"+startDay;
@@ -25,6 +26,8 @@ $(document).ready(function(){
    var daysInMonth = moment(format).daysInMonth();
 
 
+   $('#month').text(month.format('MMMM'));
+   $('#year').text(startYear);
 
    $(document).on('click', '.country-select', function(){
       country = $('.country-select').val();
@@ -65,39 +68,18 @@ $(document).ready(function(){
 
    printDayList(daysInMonth, startYear, startMonth);
 
-   function ajaxCall() {
-      $.ajax({
-         url : 'https://holidayapi.com/v1/holidays',
-         method : "GET",
-         data : {
-            key : '4283f11a-0c36-490a-a135-7df8f7c954d4',
-            country : country,
-            year : startYear,
-            month : startMonth
-         },
-         success : function(data){
-            console.log(data);
-            for (var i = 0; i < data.holidays.length; i++){
-               var hol = data.holidays[i];
-               //console.log(hol);
-               var holidayDayDate = moment(hol.date).format("D");
-               //console.log("ciao");
-               console.log(holidayDayDate);
-               $('.day').each(function(){
-                  var numberDay = $(this).children('#day-number').text();
-                  console.log(numberDay);
-                  console.log(holidayDayDate);
-                  if (numberDay == holidayDayDate) {
-                     $(this).addClass('red');
-                  }
-               });
-            }
-         },
-         error : function(e){
-            console.log(e);
-         }
-      });
-   }
+   $('.btn.back-month').click(function(){
+      var currentMonth = $('#month').text();
+      var currentYear = $('#year').text();
+      //var currentDate = moment(currentYear+"-"+);
+      console.log(moment().month(currentMonth));
+      console.log(currentMonth);
+      console.log(currentYear);
+      //console.log(currentDate);
+   });
+
+
+
 
    function printDayList(daysInMonth, year, month){
 
@@ -119,22 +101,6 @@ $(document).ready(function(){
       }
    }
 
-   // for (var i = 1; i <= daysInMonth; i++) {
-   //
-   //    if (i<10) {
-   //       i = "0"+i;
-   //       console.log("num: " + i);
-   //    }
-   //    var currDay = i.toString();
-   //
-   //    $('.days').append(
-   //       dayDivOpen +
-   //       dayNumberOpen + moment(startYear+"-"+startMonth+"-"+currDay).format("D") + dayNumberClose  +
-   //       " - " + dayNameOpen + moment(startYear+"-"+startMonth+"-"+currDay).format("dddd") + dayNameClose +
-   //       dayDivClose
-   //    );
-   //    i = parseInt(i);
-   // }
 
 
 });
---
+++ main2.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+   var country = $('.country-select').val("IT");
+   var mese = "01";
+   var year = 2017;
+
+   genDays(mese, year);
+
+   $('#forward-btn').click(function(){
+      mese = parseInt(mese) + 1;
+      if (mese == 13) {
+         mese = 1;
+         year++;
+      }
+      console.log("::" + mese);
+      console.log("::" + year);
+      genDays(mese, year);
+   });
+
+   $('#back-btn').click(function(){
+      mese = parseInt(mese) - 1;
+      if (mese == 0) {
+         mese = 12;
+         year--;
+      }
+      console.log("::" + mese);
+      console.log("::" + year);
+      genDays(mese, year);
+   });
+
+   $('.country-select *').click(function(){
+      genDays(mese, year);
+   });
+
+   function genDays(mese, year){
+      var date = moment(year+"-"+mese+"-01");
+      var giorni_mese = date.daysInMonth();
+      meseInLettere = date.format("MMMM");
+
+      console.log(":");
+      console.log($('.country-select').val());
+      $('#day-list').html('');
+
+      for (var i = 1; i <= giorni_mese; i++) {
+         var day = moment(year+"-"+mese+"-"+i)
+         $('#day-list').append("<div class='data-item' dateymd=" + day.format("YYYY-MM-DD") + ">" + i + " " + meseInLettere + " " + year + "</div>");
+      }
+
+      $.ajax({
+         url : 'https://holidayapi.com/v1/holidays',
+         method : "GET",
+         data : {
+            key : '4283f11a-0c36-490a-a135-7df8f7c954d4',
+            country : $('.country-select').val(),
+            year : year,
+            month : mese
+         },
+         success : function(data){
+            console.log(data);
+            console.log(data.holidays.length);
+            for (var i = 0; i < data.holidays.length; i++) {
+               $('.data-item').each(function(){
+                  if ( data.holidays[i].date == $(this).attr('dateymd') ){
+                     $(this).addClass('red');
+                  }
+               });
+            }
+         },
+
+         error : function(error){
+            console.log(error);
+         }
+
+      });
+
+
+   }
+
+});
